3610c9914b9b7c35e160df818371a7700314cfd4
ThangNguyen-CT <thangnguyen1333@gmail.com>
2023-03-13 20:07:00 +0700

    update

--- routes/products-router.js
+++ routes/products-router.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const product = require('../controller/product-controller');
 
-router.get('/',product.getall);
+router.get('/all',product.getall);
 router.post('/add',product.adddata);
 router.put('/update/:id',product.updatedata);
 router.delete('/delete/:id',product.deletedata);
