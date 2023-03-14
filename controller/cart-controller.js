28ead59626a98e537940ea91c23bc73ececa65f0
ThangNguyen-CT <thangnguyen1333@gmail.com>
2023-03-14 16:24:42 +0700

    update

--- controller/cart-controller.js
+++ controller/cart-controller.js
@@ -41,7 +41,7 @@ class cart{
     }
     async deletedata(req,res){
         try {
-            await Cart.findByIdAndDelete({ userId: req.params.userid });
+            await Cart.findOneAndDelete({ userId: req.params.userid });
             res.status(200).json("Cart has been deleted...");
         } catch (error) {
             console.log(error);
